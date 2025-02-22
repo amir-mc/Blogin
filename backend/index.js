368cfea6b9f727096128a9cddade5c394b9e1467
amir-mc <amirfooladi93@gmail.com>
2025-02-22 14:55:59 +0000

    updatee

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import router from "./routes/user.routes.js";
 
 const app = express();
 
-app.get("/test", (req, res) => {
-    res.status(200).send("It isdd working");
-});
+// app.get("/test", (req, res) => {
+//     res.status(200).send("It isdd working");
+// });
+app.use('/usser',router)
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
