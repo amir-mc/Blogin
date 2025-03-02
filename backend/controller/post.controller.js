78848dcd921977b07f2678e16b98fc30dde4df1c
amir-mc <amirfooladi93@gmail.com>
2025-03-02 14:36:02 +0000

    update

--- backend/controller/post.controller.js
+++ backend/controller/post.controller.js
@@ -1,5 +1,5 @@
 import postModel from "../models/post.model.js"
-
+import User from "../models/user.model.js";
 export const getposts= async (req,res)=>{
     const posts=await postModel.find()
     res.status(200).json(posts)
@@ -9,7 +9,21 @@ export const getpost= async (req,res)=>{
     res.status(200).json(posts)
 }
 export const creatpost= async (req,res)=>{
-    const newpost=new postModel(req.body)
+    const clerkUserId = req.auth.userId;
+
+  console.log(req.headers);
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) {
+    return res.status(404).json("User not found!");
+  }
+  
+    const newpost=new postModel({user:user._id, ...req.body})
         const post=await newpost.save()
         res.status(200).json(post)
     
@@ -17,7 +31,8 @@ export const creatpost= async (req,res)=>{
 }
 export const deletepost= async (req,res)=>{
    
-        const post=await postModel.findByIdAndDelete(req.params.id)
+        const post=await postModel.findByIdAndDelete({    _id: req.params.id,
+            user: user._id,})
         res.status(200).json("you destroy the post /")
     
     
--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,32 @@ import commentrouter from "./routes/comment.routes.js";
 import connectDB from "./lib/connectDB.js";
 //import webhookrouter from "./routes/webhook.routes.js";
 import webhookRouter from "./routes/webhook.routes.js";
-
+import dotenv from 'dotenv'
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors" 
+//require('dotenv').config()
+dotenv.config()
 const app = express();
+app.use(clerkMiddleware())
+app.use(cors(process.env.AUTH_CLIENT_URL))
 app.use('/webhooks', webhookRouter);
+
 app.use(express.json())
 app.use('/user',router) 
 app.use('/post',postrouter)
 app.use('/comment',commentrouter)   
-
-
+app.get('/auth',(req,res)=>{
+    const auther=req.auth
+    res.json(auther)
+})
+app.get('/',(req,res)=>{
+    const javad=process.env.test
+    res.json(`javad123 ${javad}`)
+})
+app.get('/auth',(req,res)=>{
+    const authoi=req.auth
+    res.json(authoi)
+})
 app.use((error,req,res,next)=>{
     res.status(error.status || 500)
     res.json({
@@ -23,7 +40,7 @@ app.use((error,req,res,next)=>{
     })
 })
 
-app.listen(3000,'0.0.0.0', () => {
+app.listen(3000, () => {
     connectDB()
     console.log("Server running on port 3000");
 
