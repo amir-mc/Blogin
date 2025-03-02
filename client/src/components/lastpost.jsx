a7cb764749b713b5edf1c5ed85792fe0c22aa0ac
amir-mc <amirfooladi93@gmail.com>
2025-03-02 14:34:36 +0000

    update

--- client/src/components/lastpost.jsx
+++ client/src/components/lastpost.jsx
@@ -1,6 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import Postitem from "./postitem";
+import axios from "axios";
 
+const fetchPost =async()=>{
+//const res= await axios.get('http://localhost:3000/post')
+const res= await axios.get(`${import.meta.env.VITE_API_URL}/post`)
+return res.data
+}
 const Lastpost = () => {
+    
+    const { isPending, error, data } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>fetchPost()
+      })
+    
+      if (isPending) return 'Loading...'
+    
+      if (error) return 'An error has occurred: ' + error.message
+      console.log(data)
+      
+    
     return ( 
         <div className="felx flex-col gap-12 mb-8">
                 <Postitem/>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,6 +13,13 @@ import Write from './router/write.jsx';
 import Loginpage from './router/loginpage.jsx';
 import Registerpage from './router/registerpage.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
+import {
+  QueryClient,
+  QueryClientProvider,
+
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -60,8 +67,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
 
-    
-      <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    </QueryClientProvider>
+      
       </ClerkProvider>
   </StrictMode>,
 )
